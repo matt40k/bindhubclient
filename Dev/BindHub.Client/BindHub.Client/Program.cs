2d8cbac83823e445e8d85511a5ba90ec56192d5c
matt40k_cp <SND\matt40k_cp>
2013-01-19 15:00:54 -0800

    Limit to one instance

--- Dev/BindHub.Client/BindHub.Client/Program.cs
+++ Dev/BindHub.Client/BindHub.Client/Program.cs
@@ -5,6 +5,7 @@
 
 using System;
 using System.Data;
+using System.Threading;
 using System.Windows;
 using System.Windows.Media;
 using NLog;
@@ -14,17 +15,26 @@ namespace BindHub.Client
     class Program : Window
     {
         private static NLog.Logger logger = NLog.LogManager.GetCurrentClassLogger();
-        
+
         [STAThread]
         static void Main()
         {
+            bool aIsNewInstance = false;
+
+            Mutex _mutex = new Mutex(true, "BindHub.Client", out aIsNewInstance);
+            if (!aIsNewInstance)
+            {
+                logger.Log(NLog.LogLevel.Error, "Application is already running!");
+                Environment.Exit(5);
+            }
+
             Config _config = new Config();
             if (!_config.DoesConfigExist)
             {
                 logger.Log(NLog.LogLevel.Info, "Config not found, running config ui");
                 Application application = new Application();
                 application.Run(new MainWindow(_config));
-            } 
+            }
             if (_config.DoesConfigExist)
             {
                 logger.Log(NLog.LogLevel.Info, "Config found");
