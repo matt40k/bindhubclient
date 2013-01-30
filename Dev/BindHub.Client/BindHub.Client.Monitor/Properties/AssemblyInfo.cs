d04efdba130f1ff8080bf4b94fa202f0fefe3284
matt40k_cp <SND\matt40k_cp>
2013-01-29 18:23:34 -0800

    v.1.4

--- Dev/BindHub.Client/BindHub.Client.Monitor/Properties/AssemblyInfo.cs
+++ Dev/BindHub.Client/BindHub.Client.Monitor/Properties/AssemblyInfo.cs
@@ -16,6 +16,6 @@ using System.Resources;
 [assembly: AssemblyCopyright("Copyright © BindHub.com 2013")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
-[assembly: AssemblyVersion("1.3.0.0")]
-[assembly: AssemblyFileVersion("1.3.0.0")]
+[assembly: AssemblyVersion("1.4.0.0")]
+[assembly: AssemblyFileVersion("1.4.0.0")]
 [assembly: NeutralResourcesLanguageAttribute("en-GB")]
--- Dev/BindHub.Client/BindHub.Client.Service/Properties/AssemblyInfo.cs
+++ Dev/BindHub.Client/BindHub.Client.Service/Properties/AssemblyInfo.cs
@@ -15,5 +15,5 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyCopyright("Copyright © BindHub.com 2013")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
-[assembly: AssemblyVersion("1.3.0.0")]
-[assembly: AssemblyFileVersion("1.3.0.0")]
+[assembly: AssemblyVersion("1.4.0.0")]
+[assembly: AssemblyFileVersion("1.4.0.0")]
